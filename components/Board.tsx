552544adc8c8aaac953480f3c837410ff2ffcf87
Herkamal <herkamalrandhawa@gmail.com>
2023-09-15 15:01:19 -0400

    todosgroup

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,11 +1,43 @@
+"use client";
 import React from "react";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
-
+import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
+import { useEffect } from "react";
+import { useBoardStore } from "@/store/BoardStore";
+// 2:28
 function Board() {
+  const [board, getBoard] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  const handleOnDragEnd = (result: DropResult) =>{
+    
+  }
+
   return (
-    <DragDropContext>
+    
+    <DragDropContext onDragEnd = {e => handleOnDragEnd}>
       <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => <div></div>}
+        {(provided) => 
+        <div
+        className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+          {...provided.droppableProps}
+          ref={provided.innerRef} 
+          
+        > {
+          Array.from(board.columns.entries()).map(([id, column], index) => (
+            <Column
+            key={id}
+            id={id}
+            todo={column.todos}
+            index={index}
+            />
+          ))}
+          </div>}
       </Droppable>
     </DragDropContext>
   );
---
+++ store/BoardStore.tsx
@@ -0,0 +1,19 @@
+import { getTodosGroupByColumn } from '@/lib/getTodosGroupedByColumn';
+import { create } from 'zustand'
+
+interface BoardState{
+  board: Board;
+  getBoard: () => void;
+}
+
+ export const useBoardStore = create<BoardState>((set) => ({
+  board: {
+    columns: new Map<TypedColumn, Column>()
+  },
+  getBoard: async() =>{
+    const board = await getTodosGroupByColumn();
+    set({board});
+  },
+  
+}))
+
