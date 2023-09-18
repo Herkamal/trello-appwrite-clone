a8df0a29b1413dce1e851149346da7313814a868
Herkamal <herkamalrandhawa@gmail.com>
2023-09-18 12:47:57 -0400

    openai code bug

--- app/api/generateSummary/route.tsx
+++ app/api/generateSummary/route.tsx
@@ -3,4 +3,9 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request){
   const {todos} = await request.json();
   console.log(todos);
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    temperature: 0.8,
+  })
 }
--- openai.tsx
+++ openai.tsx
@@ -1,13 +1,9 @@
-import { Configuration, OpenAIApi } from "openai";
-
-
+import { OpenAI } from "openai";
+import Configuration from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-
-const openai = new OpenAIApi(configuration);
-
+const openai = new OpenAI(configuration);
 
 export default openai;
